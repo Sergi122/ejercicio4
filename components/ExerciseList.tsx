dfda3bc3b1503fab6e5fcb8c67e8e815a5c8f464
Sergio <sergiopoma42@gmail.com>
2025-12-05 09:40:34 -0400

    list card ejercicio

---
+++ components/ExerciseList.tsx
@@ -0,0 +1,18 @@
+// components/ExerciseList.tsx
+import { ExerciseListProps } from '@/types/exercise';
+import { ExerciseCard } from './ExerciseCard';
+import { ExerciseHeader } from './ExerciseHeader';
+
+export const ExerciseList = ({ exercises }: ExerciseListProps) => {
+  return (
+    <div className="max-w-[526px] mx-auto px-4 py-6">
+      <ExerciseHeader totalExercises={exercises.length} />
+      
+      <div className="space-y-4">
+        {exercises.map((exercise) => (
+          <ExerciseCard key={exercise.id} exercise={exercise} />
+        ))}
+      </div>
+    </div>
+  );
+};
